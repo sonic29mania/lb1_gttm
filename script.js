5bed3e01cad1014446d3fd18aaf9bcf19aebbc3b
Sophia <sophioxi@gmail.com>
2026-05-07 07:33:40 +0300

    Версія 1.2

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    // --- 1. ЗАГРУЗКА ХЕДЕРА И ФУТЕРА ---
-    function includeHTML(file, elementId) {
-        const element = document.getElementById(elementId);
-        if (!element) return; // Если на странице нет этого ID, ничего не делаем
-
-        fetch(file)
-            .then(response => {
-                if (response.ok) return response.text();
-                throw new Error('Ошибка загрузки файла ' + file);
-            })
-            .then(data => {
-                element.innerHTML = data;
-            })
-            .catch(error => console.error(error));
-    }
-
-    // Вызываем загрузку
-    includeHTML('header.html', 'header-placeholder');
-    includeHTML('footer.html', 'footer-placeholder');
-
-
-    // --- 2. СЛАЙДЕР ВИДЕО ---
+    //1. СЛАЙДЕР ВІДЕО ---
     let slides = document.querySelectorAll(".slide");
     let index = 0;
-
-    // Проверяем, есть ли слайды на этой странице (чтобы не было ошибок в Контактах)
     if (slides.length > 0) {
         function changeVideo() {
             slides[index].classList.remove("active");
@@ -33,6 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
             slides[index].classList.add("active");
         }
 
-        setInterval(changeVideo, 4000); // каждые 4 секунды
+        setInterval(changeVideo, 4000); 
     }
 });
